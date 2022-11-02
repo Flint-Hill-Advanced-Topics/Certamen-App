55a6850dc945f3b24cf35c16f8047a86191e085f
CWRocks1 <ccarrico@flinthill.org>
2022-11-02 08:48:03 -0400

    Co-authored-by: sofiayuu <sofiayuu@users.noreply.github.com> Co-authored-by: Zhentarim-Cloak <Zhentarim-Cloak@users.noreply.github.com> Co-authored-by: realjun3 <realjun3@users.noreply.github.com> Co-authored-by: ItsMalek <ItsMalek@users.noreply.github.com> Co-authored-by: njpabis <njpabis@users.noreply.github.com> Co-authored-by: Wacuka <xsquared-tutoring@users.noreply.github.com>

--- GameManager.js
+++ GameManager.js
@@ -1,5 +1,28 @@
 class gameManager {
+    
     createPlayer(name) {
-        new Player(name);
+        return new Player(name);
+    }
+
+    createMod() {
+        return new Mod();
+    }
+
+    startGame() {
+        let code = randomCode();
+        let mod = createMod();
+    }
+
+    randomCode () {
+        let allCharNum = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';// length = 36, index 0 to 35
+        let roomCode = '';
+        let tempIndex = 0;
+        for (let i = 0; i < 6; i++) {
+            tempIndex = (Math.random() * 36);
+            roomCode += allCharNum.substring(tempIndex, tempIndex + 1);
+        }
+        roomcode += '√';
+        document.querySelector("#GCode")
+        return roomCode;
     }
 }
